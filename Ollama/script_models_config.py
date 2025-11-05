59ab0442677c358e17f1a2261b5147ee08fd0162
Nikoleta <nikolpant2@gmail.com>
2025-11-05 14:50:00 +0100

    Ollama models completed

--- Ollama/script_models_config.py
+++ Ollama/script_models_config.py
@@ -4,9 +4,9 @@ See full list at: https://ollama.com/library
 """
 
 MODELS = [
-   # {"name": "kimi-k2:1t-cloud"}, DONE
-   # {"name": "glm-4.6:cloud"}, DONE
-   # {"name": "gpt-oss:120b-cloud"},
+    {"name": "kimi-k2:1t-cloud"},
+    {"name": "glm-4.6:cloud"},
+    {"name": "gpt-oss:120b-cloud"},
     {"name": "qwen3-vl:235b-instruct-cloud"},
     {"name": "deepseek-v3.1:671b-cloud"},
 
